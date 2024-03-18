c905aabc2ab9bae5cb1f94223d0d228d5166fb11
rajyavardhan bithale <70558897+rajyavardhanbithale@users.noreply.github.com>
2024-03-18 21:07:12 +0530

    responsive

--- landing-page/src/app/components/Hero.jsx
+++ landing-page/src/app/components/Hero.jsx
@@ -4,10 +4,11 @@ export default function Hero() {
             <div>
 
                 <div className="relative overflow-hidden"> 
-                    <img src="assets/hero.jpg" className="brightness-[0.2] md:h-[19rem] lg:h-full" />
-                    <div className="flex flex-col absolute w-[40%] py-2.5 mission lg:top-16 top-12 text-secondary leading-8 tracking-wide animate-fade-right">
-                        <span className="text-2xl lg:text-3xl text-center p-2 m-2">Our Mission</span>
-                        <span className="text-base lg:text-xl text-center lg:p-2 lg:m-2">
+                    <img src="assets/hero.jpg" className="hidden md:block brightness-[0.2] md:h-[19rem] lg:h-full" />
+                    <img src="https://www.careerindia.com/college-photos/375x275/6733/banner-11_1652115628.jpg" className="block md:hidden brightness-[0.2] md:h-[19rem] lg:h-full" />
+                    <div className="flex flex-col absolute w-[40%] py-2.5 mission lg:top-16 md:top-12 top-0 text-secondary leading-8 tracking-wide animate-fade-right">
+                        <span className="text-base md:text-2xl lg:text-3xl text-center p-2 m-2">Our Mission</span>
+                        <span className="text-xs md:text-base lg:text-xl text-left md:text-center lg:p-2 lg:m-2">
                             Our goal is to create a vibrant innovation centre, where students from different fields come
 
                             together to explore new frontiers in technology, entrepreneurship, and creativity
