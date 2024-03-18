fdac92b8b655b6b4ac59afa1667c3656380d4bda
rajyavardhan bithale <70558897+rajyavardhanbithale@users.noreply.github.com>
2024-03-18 17:47:40 +0530

    club modified

--- landing-page/src/app/components/club/Club_info.jsx
+++ landing-page/src/app/components/club/Club_info.jsx
@@ -4,9 +4,9 @@ export default function ClubInfo(props) {
     return (
         <>
             <section class="animate-fade-up text-gray-600 body-font">
-                <div class="container mx-auto flex px-5 py-12 md:flex-row flex-col items-center">
+                <div class="container mx-auto flex px-5  md:flex-row flex-col items-center">
                     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                        <img class="object-cover object-center rounded" alt="hero" src={props?.image} />
+                        <img class=" object-cover object-center rounded" alt="hero" src={props?.image} />
                     </div>
                     <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
--- landing-page/src/app/components/club/Clubs.jsx
+++ landing-page/src/app/components/club/Clubs.jsx
@@ -8,16 +8,14 @@ import { list } from "./club_list"
 export default function Clubs() {
     const [hover, setHover] = useState(false)
     const [index, setIndex] = useState(0)
-
+    const [seed, setSeed] = useState(1);
     function handleHover(index) {
-        console.log(index);
+        // console.log(index);
         setIndex(index)
         setHover(true)
+        setSeed(Math.random());
     }
 
-    function handleBackFromChildren() {
-        setHover(false)
-    }
 
     return (
         <>
@@ -33,7 +31,7 @@ export default function Clubs() {
                         ) : (
                             <div key={idx} className="flex flex-col justify-center align-middle text-center" >
                                 <img onMouseEnter={() => handleHover(idx)} src={item.logo} alt="" className="z-0 w-56  rounded-full hover:brightness-75 transition duration-500" />
-                                <span className="text-center">{item.name}</span>
+                                <span className="text-center mt-4">{item.name}</span>
                             </div>
                         )}
 
@@ -46,16 +44,17 @@ export default function Clubs() {
                     title={list[index]?.name}
                     mission={list[index]?.mission}
                     vision={list[index]?.vision}
+                    // key={seed}
                 ></ClubInfo>
             }
 
-            <div className={`${hover ? 'p-0' : 'p-12'} w-full flex flex-wrap justify-center gap-6`}>
+            <div className={` w-full flex flex-wrap justify-center gap-6`}>
                 {list.map((item, idx) => (
                     <>
                         {hover ? (
                             <div key={idx} className="flex flex-col justify-center align-middle text-center" >
-                                <img onMouseEnter={() => handleHover(idx)} src={item.logo} alt="" className="z-0 w-56  rounded-full hover:brightness-75 transition duration-500" />
-                                <span className="text-center">{item.name}</span>
+                                <img onMouseEnter={() => handleHover(idx)} src={item.logo} alt="" className="mx-auto w-32 rounded-full hover:brightness-75 transition duration-500 " />
+                                <span className="text-center mt-0">{item.name}</span>
                             </div>
                         ) : (
                             null
