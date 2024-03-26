d6e3ad2e2cf863970eef6131ae5fc40e2aa2cdeb
rajyavardhan bithale <70558897+rajyavardhanbithale@users.noreply.github.com>
2024-03-26 20:04:53 +0530

    incharge api

---
+++ landing-page/src/app/api/db/incharge/route.jsx
@@ -0,0 +1,41 @@
+import { MongoClient } from 'mongodb';
+import { NextResponse } from 'next/server';
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+await client.connect();
+const database = client.db('SAC');
+const collection = database.collection('home');
+
+export async function GET(req, res) {
+    const data = await collection.find({}).toArray();
+    const ext = data[0]?.data;
+
+    const dict = ext.map(item => ({
+        club: item?.name,
+        name: item?.incharge,
+        position: item?.position
+    }));
+
+
+    return Response.json({ data: dict })
+
+}
+
+
+export async function POST(request) {
+    const body = await request.json();
+
+    const result = await collection.updateOne(
+        {},
+        { $push: { id: body.id } }
+    );
+
+    console.log(body.id);
+
+    return NextResponse.json({ response: result.modifiedCount });
+}
+
