335fb4ebf7f9cb1807579ccb345a9db9eedb3c34
rajyavardhan bithale <70558897+rajyavardhanbithale@users.noreply.github.com>
2024-04-29 02:10:30 +0530

    modify

--- landing-page/src/app/club/[id]/page.jsx
+++ landing-page/src/app/club/[id]/page.jsx
@@ -45,9 +45,15 @@ export default async function ClubsPage(request) {
 
     return (
         <>
-            <Navbar></Navbar>
+            <div className="flex justify-end">
+                <div className="lg:w-[82%] sm:w-[75%] w-[100%]">
+                    <Navbar></Navbar>
+                </div>
+            </div>
 
-            <div className="mt-16 flex w-full min-h-screen">
+
+
+            <div className="md:mt-16 mt-20 flex w-full min-h-screen">
                 <div className="block sm:hidden fixed bottom-0 w-full z-50">
                     <BottomBarClub mission={data?.data?.mission}
                         vision={data?.data?.vision}
